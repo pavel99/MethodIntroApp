5da157c47837e101f581510e63af8ed7249aa0f8
pavel99 <mailforgithub@gmail.com>
2015-05-13 16:49:59 +0600

    First commit

---
+++ MethodIntroApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodIntroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aValue = GetData("Pavel","Fahmi");
+            Console.WriteLine(aValue);
+
+            int bValue=
+        }
+        //Method without parameter
+
+        //static string Getdata()
+        //{
+        //    return "Hello world";
+        //}
+
+        //Method With Parameter
+        static string GetData(string firstString, string secondString)
+        {
+            return firstString + " " + secondString;
+        }
+
+        static int AddData(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
